6071580705bd936d06e1257d061956f090649126
amartinsavia <49476354+amartinsavia@users.noreply.github.com>
2019-04-10 16:14:23 +0200

    Update apiToDo.js

--- apiToDo.js
+++ apiToDo.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const body_parser = require("body-parser");
 const path = require('path');
+var port = process.env.PORT || 3000;
 
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(body_parser.json());
@@ -113,6 +114,6 @@ app.put('/task/:id', function (req, res) {
     }
 });
 
-app.listen(3000, () => {
+app.listen(port, function() => {
     console.log("El servidor está inicializado en el puerto 3000");
 });
